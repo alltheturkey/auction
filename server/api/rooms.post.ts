5b64be543ebf5027d9e5201900f8be821eb21bc2
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-12 20:24:00 +0900

    refactor(backend): import path

--- server/api/rooms.post.ts
+++ server/api/rooms.post.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { prismaErrorHandler } from '../lib/prismaErrorHandler';
+import { prismaErrorHandler } from '~/server/lib/prismaErrorHandler';
 
 const prisma = new PrismaClient();
 

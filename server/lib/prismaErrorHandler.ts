9f3e9517b5cc86866759068a53eb9f06c42c4aa0
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-13 10:18:06 +0900

    fix(backend): prismaErrorHandler

--- server/lib/prismaErrorHandler.ts
+++ server/lib/prismaErrorHandler.ts
@@ -3,10 +3,7 @@ import type { Prisma } from '@prisma/client';
 export const prismaErrorHandler = (
   err: Prisma.PrismaClientKnownRequestError,
 ) => {
-<<<<<<< HEAD
-=======
   console.error(err);
->>>>>>> 1668cab87ddc23934f32ef8867408ff09877afb6
   const statusCodeMap = new Map<
     Prisma.PrismaClientKnownRequestError['code'],
     number

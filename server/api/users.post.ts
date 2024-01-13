debfd275b4d7572d02ef7d82b52af17ac8dff366
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-13 12:37:07 +0900

    feat(backend): add zodErrorHandler

--- server/api/users.post.ts
+++ server/api/users.post.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { z } from 'zod';
 import { prismaErrorHandler } from '~/server/lib/prismaErrorHandler';
+import { zodErrorHandler } from '~/server/lib/zodErrorHandler';
 
 const prisma = new PrismaClient();
 
@@ -8,7 +9,9 @@ export default defineEventHandler(async (event) => {
   const schema = z.object({
     name: z.string(),
   });
-  const userRequest = schema.parse(await readBody(event));
+  const userRequest = await schema
+    .parseAsync(await readBody(event))
+    .catch(zodErrorHandler);
   const user = await prisma.user
     .create({
       data: userRequest,

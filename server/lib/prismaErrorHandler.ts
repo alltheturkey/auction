61cd52f01fe21f6e274e7ab0d257cc638dbdd7d9
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-13 09:46:30 +0900

    feat(backend): GET /rooms

---
+++ server/lib/prismaErrorHandler.ts
@@ -0,0 +1,16 @@
+import type { Prisma } from '@prisma/client';
+
+export const prismaErrorHandler = (
+  err: Prisma.PrismaClientKnownRequestError,
+) => {
+  console.error(err);
+  const statusCodeMap = new Map<
+    Prisma.PrismaClientKnownRequestError['code'],
+    number
+  >([['P2025', 404]]);
+
+  throw createError({
+    statusCode: statusCodeMap.get(err.code) ?? 500,
+    message: err.message,
+  });
+};

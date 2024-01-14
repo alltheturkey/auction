ee78140dfeb8fe6b49bcb26e6e1e78293deb617a
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-14 21:49:43 +0900

    feat(backend): PUT /rooms/{roomId}

---
+++ server/api/rooms/[roomId].put.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+import { broadcastRoom } from '~/server/lib/broadcastRoom';
+import { prismaErrorHandler } from '~/server/lib/prismaErrorHandler';
+import { shuffleArr } from '~/server/lib/shuffleArr';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const roomId = event.context.params!.roomId;
+  const { users } = await prisma.room
+    .findUniqueOrThrow({
+      where: { id: roomId },
+      include: {
+        users: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+    .catch(prismaErrorHandler);
+
+  // turnUserIdを設定
+  const shuffledUsers = shuffleArr(users);
+  await prisma.room
+    .update({
+      where: {
+        id: roomId,
+      },
+      data: {
+        turnUserId: shuffledUsers[0].id,
+        auctionId: null,
+        tradeId: null,
+        userOrder: shuffledUsers.map(({ id }) => id),
+      },
+    })
+    .catch(prismaErrorHandler);
+
+  // roomに属するuserのcardを初期化
+  await prisma.userCard.deleteMany({
+    where: {
+      userId: {
+        in: users.map(({ id }) => id),
+      },
+    },
+  });
+
+  // userに初期お金を配る
+  for (const user of users) {
+    await prisma.userCard.createMany({
+      data: [
+        // 0: 2枚
+        { userId: user.id, cardId: 1 },
+        { userId: user.id, cardId: 1 },
+        // 10: 4枚
+        { userId: user.id, cardId: 2 },
+        { userId: user.id, cardId: 2 },
+        { userId: user.id, cardId: 2 },
+        { userId: user.id, cardId: 2 },
+        // 50: 1枚
+        { userId: user.id, cardId: 3 },
+      ],
+    });
+  }
+
+  await broadcastRoom(roomId);
+
+  return;
+});
--- server/lib/broadcastRoom.ts
+++ server/lib/broadcastRoom.ts
@@ -1,1 +1,34 @@
-export const broadcastRoom = (roomId: string) => {};
+import { PrismaClient } from '@prisma/client';
+import { prismaErrorHandler } from '~/server/lib/prismaErrorHandler';
+
+const prisma = new PrismaClient();
+
+// [] ws serverにroom(リレーション含む)をPOSTする、responce.okでない場合はthrow createError
+export const broadcastRoom = async (roomId: string) => {
+  const room = await prisma.room
+    .findUniqueOrThrow({
+      where: { id: roomId },
+      select: {
+        turnUser: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        users: {
+          select: {
+            id: true,
+            name: true,
+            userCards: {
+              select: {
+                card: true,
+              },
+            },
+          },
+        },
+      },
+    })
+    .catch(prismaErrorHandler);
+
+  console.log(JSON.stringify(room, null, 2));
+};

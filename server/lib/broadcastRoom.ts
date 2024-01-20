524a0cc3ea43aa6f8c9dc5651b9e99bfbce0b3a8
Tksi <24800668+Tksi@users.noreply.github.com>
2024-01-20 23:43:02 +0900

    feat(backend): fetch wsApi

--- server/lib/broadcastRoom.ts
+++ server/lib/broadcastRoom.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { prismaErrorHandler } from '~/server/lib/prismaErrorHandler';
 
 const prisma = new PrismaClient();
+const runtimeConfig = useRuntimeConfig();
 
 // [] ws serverにroom(リレーション含む)をPOSTする、responce.okでない場合はthrow createError
 export const broadcastRoom = async (roomId: string) => {
@@ -41,14 +42,10 @@ export const broadcastRoom = async (roomId: string) => {
         },
         trade: {
           select: {
-            targetUser: {
-              select: {
-                id: true,
-                name: true,
-              },
-            },
+            targetUserId: true,
             targetUserAnimalUserCardIds: true,
             turnUserAnimalUserCardIds: true,
+            confirmedUserId: true,
             tradeBet: {
               select: {
                 userId: true,
@@ -66,7 +63,27 @@ export const broadcastRoom = async (roomId: string) => {
     })
     .catch(prismaErrorHandler);
 
-  console.log(JSON.stringify(room, null, 2));
+  await fetch(new URL(`/${roomId}`, runtimeConfig.wsApi).href, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(room),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw createError({
+          statusCode: 500,
+          message: 'Failed to broadcast',
+        });
+      }
+    })
+    .catch((err: Error) => {
+      throw createError({
+        statusCode: 500,
+        message: err.message,
+      });
+    });
 
-  return room;
+  return;
 };

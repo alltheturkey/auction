a57d93bdaf50bc35c75768c9c1321f9a1064da35
Tksi <24800668+Tksi@users.noreply.github.com>
2024-02-12 10:59:53 +0900

    feat(frontend): meta settings

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <slot />
+  </v-app>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'auction',
+  meta: [
+    {
+      name: 'robots',
+      content: 'noindex nofollow',
+    },
+  ],
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,32 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+  spaLoadingTemplate: false,
   experimental: {
     typedPages: true,
   },
-  modules: ['vuetify-nuxt-module'],
+  modules: ['vuetify-nuxt-module', '@nuxtjs/robots'],
   runtimeConfig: {
     wsApi: process.env.NUXT_WS_API ?? 'http://localhost:4567',
     public: {
       ws: process.env.NUXT_PUBLIC_WS ?? 'ws://localhost:4567',
+    },
+  },
+  robots: {
+    rules: {
+      UserAgent: '*',
+      Disallow: '/',
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐔</text></svg>',
+        },
+      ],
     },
   },
 });
